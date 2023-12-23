7d8e2a6843e22ecab60abd8abc788e1297f5b134
mvaldes <mvaldes.revilla@gmail.com>
2023-12-23 12:55:41 -0600

    adding flake

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "Tools for Ansible";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  outputs = { nixpkgs, self }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system;};
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+         ansible
+         ansible-lint
+        ];
+      };
+    };
+}
+
