8c3b586b0c34a0da5dc0c4aafca5fa4f226da8d5
mvaldes <mvaldes.revilla@gmail.com>
2024-02-26 21:52:04 -0600

    update: a lot of stuff is done via nix now

--- flake.nix
+++ flake.nix
@@ -5,13 +5,14 @@
   outputs = { nixpkgs, self }:
     let
       system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system;};
+      pkgs = import nixpkgs { inherit system; };
     in
     {
       devShells.${system}.default = pkgs.mkShell {
         buildInputs = with pkgs; [
-         ansible
-         ansible-lint
+          ansible
+          ansible-lint
+          molecule
         ];
       };
     };
