deba1b7934597fde69036079076cfffbef8e6b38
mvaldes <mvaldes.revilla@gmail.com>
2024-06-26 16:10:41 -0500

    adding molecule and homelab base playbook

--- flake.nix
+++ flake.nix
@@ -6,30 +6,19 @@
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
-      molecule-docker = pkgs.stdenv.mkDerivation
-        rec {
-          pname = "molecule-plugins[docker]";
-          version = "23.5.3";
-          src = pkgs.fetchFromGitHub {
-            owner = "ansible-community";
-            repo = "molecule-plugins";
-            rev = "v${version}";
-            sha256 = "sha256-v0yj2RztsRoWIUw8FYLuMMZihY3R9eJrA8ikQpEpwJ0=";
-          };
-          buildInputs = [ pkgs.python3 ];
-        };
     in
     {
       devShells.${system}.default = pkgs.mkShell {
-        buildInputs = with pkgs;
-          [
-            ansible
-            ansible-lint
-            molecule
-            molecule-docker
-            python311Packages.kubernetes
-            python311Packages.pip
-          ];
+        buildInputs = with pkgs;[
+          ansible
+          ansible-lint
+          python311Packages.kubernetes
+          python311Packages.pip
+          python3Packages.virtualenv
+          python3Packages.molecule-plugins
+          python3Packages.molecule-plugins.optional-dependencies.docker
+          python3Packages.molecule
+        ];
       };
     };
 }
